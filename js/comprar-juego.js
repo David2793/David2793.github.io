23a8fd4d08c2ffd034582a0935d7609e1caab8c8
Davif <sdavidantoniog@gmail.com>
2018-11-17 15:54:11 -0300

    hoy

--- js/comprar-juego.js
+++ js/comprar-juego.js
@@ -1,6 +1,40 @@
-document.addEventListener("DOMContentLoaded", function(){
+var comun = 0;
+var premium = 0;
+var especial = 0;
+var subtotal = 0;
+var iva = 0;
+var envio = 0;
+var total = 0;
+var cantidad = 0;
+var costoEnvio = 0;
+
+total = total.toFixed(2);
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    function calcularCostos() {
+        comun = cantidad * precioUnitario * 0.005;
+        especial = cantidad * precioUnitario * 0.02;
+        premium = cantidad * precioUnitario * 0.05;
+        subtotal = (cantidad * precioUnitario) + costoEnvio;
+        iva = subtotal * 0.22;
+        total = subtotal + iva;
+
+        spanSubtotal.innerHTML = "$" + Number((subtotal).toFixed(1));
+        spanTotal.innerHTML = "$" + Number((total).toFixed(1));
+        spanTransporte1.innerHTML = "$" + comun;
+        spanTransporte2.innerHTML = "$" + especial;
+        spanTransporte3.innerHTML = "$" + premium;
+        spanIva.innerHTML = "$" + Number((iva).toFixed(1));
+    }
+
     var campoNombre = document.getElementById("nombre");
     var campoCI = document.getElementById("dni");
+    var campodia = document.getElementById("dianacimiento");
+    var campomes = document.getElementById("fechaNacimientoMes");
+    var campoanio = document.getElementById("aniodenacimiento");
+    var tarjetaanio = document.getElementById("tarjetaanio");
+    var tarjetames = document.getElementById("tarjetames")
     var campoTelefonoMovil = document.getElementById("movil");
     var campoEmail = document.getElementById("email");
     var campoRepitaelEmail = document.getElementById("email2");
@@ -10,157 +44,164 @@ document.addEventListener("DOMContentLoaded", function(){
     var radiojuegodigital = document.getElementById("digital");
     var radiojuegofisico = document.getElementById("fisico");
     var botoncomprar = document.getElementById("boton-comprar");
+    var transportecomun = document.getElementById("radioComun");
+    var transporteespecial = document.getElementById("radioEspecial");
+    var transportepremium = document.getElementById("radioPremium");
+    var comun = document.getElementById("enviofisico");
 
 
-    radiojuegodigital.addEventListener("change", function(){
-        domicilio.style.display="none";
+    radiojuegodigital.addEventListener("change", function() {
+        domicilio.style.display = "none";
+        localidad.style.display = "none";
+        codigoPostal.style.display = "none";
+        dpto.style.display = "none";
+        enviofisico.style.display = "none";
     });
 
-    radiojuegofisico.addEventListener("change", function(){
-        domicilio.style.display="block";
-    });
+    radiojuegofisico.addEventListener("change", function() {
+        domicilio.style.display = "block";
+        localidad.style.display = "block";
+        codigoPostal.style.display = "block";
+        dpto.style.display = "block";
+        enviofisico.style.display = "block";
 
-    radiojuegodigital.addEventListener("change", function(){
-        localidad.style.display="none";
-    });
-
-    radiojuegofisico.addEventListener("change", function(){
-        localidad.style.display="block";
-    });
-
-    radiojuegodigital.addEventListener("change", function(){
-        codigoPostal.style.display="none";
-    });
-
-    radiojuegofisico.addEventListener("change", function(){
-        codigoPostal.style.display="block";
-    });
-
-    radiojuegodigital.addEventListener("change", function(){
-        dpto.style.display="none";
-    });
-
-    radiojuegofisico.addEventListener("change", function(){
-        dpto.style.display="block";
     });
 
 
+    transportecomun.addEventListener("change", function() {
+        costoEnvio = subtotal * 0.005;
+        calcularCostos();
     });
 
-    //PRODUCTO PARA COMPRAR:
+    transporteespecial.addEventListener("change", function() {
+        costoEnvio = subtotal * 0.02;
+        calcularCostos();
+    });
+
+    transportepremium.addEventListener("change", function() {
+        costoEnvio = subtotal * 0.05;
+        calcularCostos();
+    });
+
+
     var productos = ["Counter Strike 1.6 (PC)"];
-  	var imgGrandes = [""];
-  	var imgPeque = ["counter.jpg"];
-  	var precio = [150];
-  	var stock = [10];
-  	var precioTransportecomun = 0.05;
-  	var precioTransportecomun = 0.2;
-  	var precioTransportecomun = 0.5;
-  	var IVA = 0.22;
-  	var uniUser;
+    var imgGrandes = ["counter.jpg"];
+    var imgPeque = ["counter.jpg"];
 
-window.onload = function(){
+
     var juego = document.getElementsByName("juego");
-    for (i in productos){
-    juego[i].innerHTML = '<a id="imgG'+i+'" href="' +imgGrandes[i]+ '"><img id="imgP'+i+'" class="imagen" src="' +imgPeque[i]+ '"></a><div <span id="counterstrike'+i+'">' +productos[i]+ '</span>: <span id="pre'+i+'">' +precio[i]+ '$</span></b></div><div class="stock">Hay en stock <span id="uni'+i+'">' +stock[i]+ '</span> unidades,<br/>Selecciona la cantidad de juegos: <input class="uniBien" type="number" id="uniUser'+i+'" name="uniUser" value="0" size="4" /></div>';
-  }
+    for (i in productos) {
+        juego[i].innerHTML = '<a id="imgG' + i + '" href="' + imgGrandes[i] + '"><img id="imgP' + i + '" class="imagen" src="' + imgPeque[i] + '"></a></div>';
+    }
 
+    //DIA fecha de nacimiento:
     var fecha = new Date();
-    //Fecha de nacimiento DIA:
 
-    for (var i=1;i<=31;i++){
-        document.getElementById("dianacimiento").innerHTML = document.getElementById("dianacimiento").innerHTML + '<option value="' +i+ '">' +i+ '</option>';
+
+    for (var i = 1; i <= 31; i++) {
+        document.getElementById("dianacimiento").innerHTML = document.getElementById("dianacimiento").innerHTML + '<option value="' + i + '">' + i + '</option>';
     }
 
     var vdianacimiento = document.getElementById("dianacimiento").selectedIndex;
-    if( vdianacimiento == null || vdianacimiento == 0 ) {
-    todoBien=false;
+    if (vdianacimiento == null || vdianacimiento == 0) {
+        todoBien = false;
         document.getElementById("dianacimiento").className = "textMal";
-    }
-    else{
+    } else {
         document.getElementById("dianacimiento").className = "textBien";
-        }
+    }
 
 
+    //AÑO de nacimiento:
     var anio = fecha.getFullYear();
-    //Fecha de nacimiento AÑO:
-
-    for (var i=anio;i>=(anio-100);i--){
-			document.getElementById("aniodenacimiento").innerHTML = document.getElementById("aniodenacimiento").innerHTML + '<option value="' +i+ '">' +i+ '</option>';
-		}
-
-    //Fecha de nacimiento AÑO:
-			var vaniodenacimiento = document.getElementById("aniodenacimiento").selectedIndex;
-			if( vaniodenacimiento == null || vaniodenacimiento == 0 ) {
-				todoBien=false;
-				document.getElementById("aniodenacimiento").className = "textMal";
-			}
-			else{
-				document.getElementById("aniodenacimiento").className = "textBien";
-			}
-
-      //Telefono:
-  			var vMovil = document.getElementById("movil").value;
-  			if( !(/^\d{9}$/.test(vMovil))  ) {
-  				todoBien=false;
-  				document.getElementById("movil").className = "textMal";
-  			}
-  			else{
-  				document.getElementById("movil").className = "textBien";
-  			}
-        //email:
-    			var vEmail1 = document.getElementById("email1").value;
-    			var vEmail2 = document.getElementById("email2").value;
-
-    			//email 1
-    			if( !(/^\w+([-.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(vEmail1)) ) {
-    				todoBien=false;
-    				document.getElementById("email1").className = "textMal";
-    			}
-    			else{
-    				document.getElementById("email1").className = "textBien";
-    			}
-
-    			//email 2
-    			if( !(/^\w+([-.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(vEmail2)) ) {
-    				todoBien=false;
-    				document.getElementById("email2").className = "textMal";
-    			}
-    			else{
-    				document.getElementById("email2").className = "textBien";
-    			}
-
-          //Comparacion email 1 y 2
-    			if (vEmail1 != vEmail2){
-    				todoBien=false;
-    				document.getElementById("email2").className = "textMal";
-    			}
-};
-          //FUNCION DE VALIDACION DE UNIDADES:
-          	function validaLasUnidades(elEvento) {
-
-          		var todoBien = true;
-          		uniUser = document.getElementsByName("uniUser");
 
 
-              for (i in productos){
+    for (var i = anio; i >= (anio - 100); i--) {
+        document.getElementById("aniodenacimiento").innerHTML = document.getElementById("aniodenacimiento").innerHTML + '<option value="' + i + '">' + i + '</option>';
+    }
 
-              if ( uniUser[i].value == "" || uniUser[i].value > stock[i] || uniUser[i].value < 0 ){
+    var vaniodenacimiento = document.getElementById("aniodenacimiento").selectedIndex;
+    if (vaniodenacimiento == null || vaniodenacimiento == 0) {
+        todoBien = false;
+        document.getElementById("aniodenacimiento").className = "textMal";
+    } else {
+        document.getElementById("aniodenacimiento").className = "textBien";
+    }
 
-              todoBien = false;
-              uniUser[i].className = "uniMal";
+    //Celular:
+    var vMovil = document.getElementById("movil").value;
+    if (!(/^\d{9}$/.test(vMovil))) {
+        todoBien = false;
+        document.getElementById("movil").className = "textMal";
+    } else {
+        document.getElementById("movil").className = "textBien";
+    }
+    //email:
+    var vEmail1 = document.getElementById("email1").value;
+    var vEmail2 = document.getElementById("email2").value;
 
-   //Con solo un error se para la validacion de unidades:
-   return;
- }
- else{
-   todoBien = true;
-   uniUser[i].className = "uniBien";
- }
-}
+    //email 1
+    if (!(/^\w+([-.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(vEmail1))) {
+        todoBien = false;
+        document.getElementById("email1").className = "textMal";
+    } else {
+        document.getElementById("email1").className = "textBien";
+    }
 
-//Si no ha habido ni un solo error, se ejecuta la siguiente funcion que se encarga de cargar el carro de la compra:
-if (todoBien){
- calculaElTotal();
-}
-}
+    //email 2
+    if (!(/^\w+([-.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(vEmail2))) {
+        todoBien = false;
+        document.getElementById("email2").className = "textMal";
+    } else {
+        document.getElementById("email2").className = "textBien";
+    }
+
+    //Comparacion email 1 y 2
+    if (vEmail1 != vEmail2) {
+        todoBien = false;
+        document.getElementById("email2").className = "textMal";
+    }
+
+
+    //Tarjeta de credito:
+    for (var i = 1; i <= 12; i++) {
+        document.getElementById("tarjetames").innerHTML = document.getElementById("tarjetames").innerHTML + '<option value="' + i + '">' + i + '</option>';
+    }
+
+    for (var i = anio; i <= (anio + 21); i++) {
+        document.getElementById("tarjetaanio").innerHTML = document.getElementById("tarjetaanio").innerHTML + '<option value="' + i + '">' + i + '</option>';
+    }
+
+    //Fecha de tarjeta MES:
+    var tarjetames = document.getElementById("tarjetames").selectedIndex;
+    if (tarjetames == null || tarjetames == 0) {
+        todoBien = false;
+        document.getElementById("tarjetames").className = "textMal";
+    } else {
+        document.getElementById("tarjetames").className = "textBien";
+    }
+
+    //Fecha de tarjeta AÑO:
+    var tarjeanio = document.getElementById("tarjetaanio").selectedIndex;
+    if (tarjetaanio == null || tarjetames == 0) {
+        todoBien = false;
+        document.getElementById("tarjetaanio").className = "textMal";
+    } else {
+        document.getElementById("tarjetaanio").className = "textBien";
+    }
+
+    var precioUnitario = 150;
+    var campoCantidad = document.getElementById("campocantidad");
+    var spanSubtotal = document.getElementById("subtotal");
+    var spanTotal = document.getElementById("total");
+    var spanTransporte1 = document.getElementById("comun");
+    var spanTransporte2 = document.getElementById("especial");
+    var spanTransporte3 = document.getElementById("premium");
+    var spanIva = document.getElementById("iva");
+
+    campoCantidad.addEventListener("change", function() {
+        cantidad = campoCantidad.value;
+        cantidad = parseInt(cantidad);
+        calcularCostos();
+
+    });
+});
